cf153ed87d81b685ef16d817f61f9585ae155801
Clemens Thees (ext.) <Clemens.CT.Thees@partner.bmw.de>
2023-06-29 17:56:19 +0200

    ADD primary service

---
+++ src/main/java/de/maibornwolff/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,13 @@
+package de.maibornwolff.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the primary bean!";
+    }
+}
